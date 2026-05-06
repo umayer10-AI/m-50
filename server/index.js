5343ea2885e73be89565a912fe60897012b805a4
umayer10-AI <mdumayer10@gmail.com>
2026-05-06 18:26:19 +0600

    add

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+// mongodb+srv://server:A5Vuv8R9nHmNtxPw@cluster0.ywso1qs.mongodb.net/?appName=Cluster0
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World Umayer')
 })
 
 app.listen(port, () => {
