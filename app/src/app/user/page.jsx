b8f57ad3a5e56096b704678bbe610b6b33eca8c9
umayer10-AI <mdumayer10@gmail.com>
2026-05-06 19:23:12 +0600

    add

--- app/src/app/user/page.jsx
+++ app/src/app/user/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { getUser } from '../lib/data';
 
-const page = () => {
+const page = async () => {
+
+    const data = await getUser()
+
     return (
         <div>
             User management
+            <h2>Data: {data.length}</h2>
         </div>
     );
 };
