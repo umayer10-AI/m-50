b55bdbb2707f7a82b84f1a5e3c59f57a55eadeac
umayer10-AI <mdumayer10@gmail.com>
2026-05-06 19:45:12 +0600

    add

--- app/src/app/lib/data.js
+++ app/src/app/lib/data.js
@@ -2,3 +2,10 @@ export const getUser = async () => {
     const res = await fetch("http://localhost:5000/user")
     return res.json()
 }
+
+export const getId = async (id) => {
+    // const res = await fetch("http://localhost:5000/user")
+    // const data = await res.json()
+    // const f = data.find(v => v._id === id)
+    // return f
+}
